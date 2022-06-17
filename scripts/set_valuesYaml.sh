f82e39b5a15aafeaec45da63ac4eacc38980eb56
vishal <vmunde62@gmail.com>
2022-06-17 11:25:30 +0530

    added scripts

--- scripts/set_valuesYaml.sh
+++ scripts/set_valuesYaml.sh
@@ -1,9 +1,9 @@
-if [ $clusterName = cluster1 ];then
-valuesYaml='c1values.yaml';
-elif [ $clusterName = cluster2 ];then
-valuesYaml='c2values.yaml';
-elif [ $clusterName = cluster3 ];then
-valuesYaml='c3values.yaml';
+if [ $clusterName = cluster1 ]; then
+    valuesYaml='c1values.yaml'
+elif [ $clusterName = cluster2 ]; then
+    valuesYaml='c2values.yaml'
+elif [ $clusterName = cluster3 ]; then
+    valuesYaml='c3values.yaml'
 fi
 
 echo $valuesYaml
